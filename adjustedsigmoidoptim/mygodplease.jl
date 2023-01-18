35c0bf1731beada0d24ca0d3afafb39942091eef
vbprojects <bhatnagarvarun2020@gmail.com>
2023-01-18 00:54:03 -0500

    finally figured out svm from scratch

--- adjustedsigmoidoptim/mygodplease.jl
+++ adjustedsigmoidoptim/mygodplease.jl
@@ -1,42 +1,32 @@
-using JuMP, MLJ, Tables, LinearAlgebra, HiGHS, Plots
+using JuMP, MLJ, Tables, LinearAlgebra, HiGHS, Plots, COSMO
 
-rbf(x, y) = exp(-norm(x - y)^2 / 10)
+rbf(x, y) = exp(-norm(x - y)^2 / .1)
 sigmoid(x) = 1 / (1 + exp(-x))
 
 N = 1000
 D = make_moons(N)
 X = D[1] |> Tables.matrix |> eachrow .|> collect
-Y = D[2] .== 1 .|> Float64
+Y = (D[2] .== 1 .|> Float64) .* 2 .- 1
 d = length(X[1])
 w = zeros(2)
 S = ones(N)
-
-K = [rbf(X[i], X[j]) for i in 1:N, j in 1:N] |> eachrow .|> collect
-C = 1.0
-# @variable(model, X[1:N, 1:d])
-# @variable(model, Y[1:N])
+lam = 10
 
 begin
-    model = Model(HiGHS.Optimizer)
-    @variable(model, w[1:N])
-    @variable(model, b)
-    @variable(model, S[1:N])
-
-    for i in 1:N
-        @constraint(model, Y[i] * (w' * K[i] + b) + C * S[i] >= 1)
-    end
-
-    @objective(model, Min, sum(S))
-
+    model = JuMP.Model(COSMO.Optimizer)
+    @variable(model, 0 <= c[1:N] <= 1/(2 * N * lam))
+    @constraint(model, sum(c[i] * Y[i] for i in 1:N) == 0)
+    @objective(model, Max, -1/2 * sum(Y[i] * c[i] * rbf(X[i], X[j]) * Y[j] * c[j] for i in 1:N, j in 1:N) + sum(c[i] for i in 1:N))
     optimize!(model)
 end
-w = value.(w)
 
-function predict(x)
-    return w' * [rbf(x, K[i]) for i in 1:N] + value(b)
+function predict(z)
+    C = value.(c)
+    sum(C[i] * Y[i] * rbf(X[i], z) for i in 1:N)
 end
-findall(==(0), Y)
-pred = predict.(K) .> .5
-co = Y .!= pred
+
 Xv = D[1] |> Tables.matrix
-scatter(Xv[:, 1], Xv[:, 2], group = co)
+wrong = (x -> x == 1 ? "wrong" : "right").(sign.(predict.(X)) .!= Y) 
+scatter(Xv[:, 1], Xv[:, 2], group = wrong)
+
+value.(c) |> sort |> reverse |> plot
