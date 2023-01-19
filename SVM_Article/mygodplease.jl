f62f19c7a9d0b60add67784ee0c960112edfe04e
vbprojects <bhatnagarvarun2020@gmail.com>
2023-01-19 14:13:33 -0500

    finished SVM training with JuMP working on surrogate sigmoid-esque optimization finished article implemented pegasos algo

--- SVM_Article/mygodplease.jl
+++ SVM_Article/mygodplease.jl
@@ -1,4 +1,4 @@
-using JuMP, MLJ, Tables, LinearAlgebra, HiGHS, Plots, COSMO
+using JuMP, MLJ, Tables, LinearAlgebra, Plots, COSMO
 
 rbf(x, y) = exp(-norm(x - y)^2 / .1)
 sigmoid(x) = 1 / (1 + exp(-x))
@@ -10,23 +10,31 @@ Y = (D[2] .== 1 .|> Float64) .* 2 .- 1
 d = length(X[1])
 w = zeros(2)
 S = ones(N)
-lam = 10
+lam = .1
 
 begin
     model = JuMP.Model(COSMO.Optimizer)
     @variable(model, 0 <= c[1:N] <= 1/(2 * N * lam))
     @constraint(model, sum(c[i] * Y[i] for i in 1:N) == 0)
-    @objective(model, Max, -1/2 * sum(Y[i] * c[i] * rbf(X[i], X[j]) * Y[j] * c[j] for i in 1:N, j in 1:N) + sum(c[i] for i in 1:N))
+    @objective(model, Max, -1/2 * sum(Y[i] * c[i] * ρ(X[i], X[j]) * Y[j] * c[j] for i in 1:N, j in 1:N) + sum(c[i] for i in 1:N))
     optimize!(model)
 end
 
-function predict(z)
+ρ(x, y) = rbf(x, y)
+
+function predict(Z, I)
     C = value.(c)
-    sum(C[i] * Y[i] * rbf(X[i], z) for i in 1:N)
+    pYs = zeros(length(Z))
+    for j in 1:length(Z)
+        pYs[j] = sum(C[i] * Y[i] * ρ(X[i], Z[j]) for i in I) |> sign
+    end
+    return pYs
 end
 
 Xv = D[1] |> Tables.matrix
-wrong = (x -> x == 1 ? "wrong" : "right").(sign.(predict.(X)) .!= Y) 
+inds = sortperm(value.(c))
+wrong = (x -> x == 1 ? "wrong" : "right").(sign.(predict(X,inds)) .!= Y) 
 scatter(Xv[:, 1], Xv[:, 2], group = wrong)
-
 value.(c) |> sort |> reverse |> plot
+
+# scatter(Xv[:, 1], Xv[:, 2], group = Y, legend = false)
